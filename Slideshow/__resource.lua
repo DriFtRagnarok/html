a163af5ffc11c185bedbb7aabba876b2324e69ad
Logan <49781823+DriFtRagnarok@users.noreply.github.com>
2019-07-19 11:59:04 -0500

    Add files via upload

---
+++ Slideshow/__resource.lua
@@ -0,0 +1,16 @@
+loadscreen 'index.html';
+
+files{
+    'FBG1.jpg',
+    'FBG2.jpg',
+    'FBG3.jpg',
+    'FBG4.jpg',
+    'index.html',
+    'style.css',
+    'background.png',
+    'song1.mp3',
+    'song2.mp3',
+    'song3.mp3',
+}
+
+resouce_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
