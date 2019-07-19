c8c6e74b8ad0e724b2c1bb708aa5cc7e4e7bdc31
Logan <49781823+DriFtRagnarok@users.noreply.github.com>
2019-07-19 09:31:33 -0500

    Add files via upload

---
+++ MRP-Load/__resource.lua
@@ -0,0 +1,14 @@
+----- DriFt Raganrok -----
+
+resouce_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+files{
+    'files/index.html',
+    'files/style.css',
+    'files/load.gif',
+    'files/main.js',
+    'files/background.png',
+    'files/closer.mp3'
+}
+
+loadscreen 'files/index.html'
---
+++ MRP-Load/files/main.js
@@ -0,0 +1,1 @@
+/*** DriFt Ragnarok ***/
